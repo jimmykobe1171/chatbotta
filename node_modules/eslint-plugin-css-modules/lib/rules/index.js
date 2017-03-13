5df9096f26ac347ab63a1c6582a8df282128eb29
Ethan Yuxuan Chen <ychen18@nd.edu>
2017-03-13 11:41:59 -0400

    remove node modules

--- node_modules/eslint-plugin-css-modules/lib/rules/index.js
+++
@@ -1,7 +0,0 @@
-import noUnusedClass from './no-unused-class';
-import noUndefClass from './no-undef-class';
-
-export default {
-  'no-unused-class': noUnusedClass,
-  'no-undef-class': noUndefClass,
-};
