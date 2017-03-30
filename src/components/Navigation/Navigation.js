70a653c6228417628ce71ad3b14c435d12a1dd7f
TingMeng <tl2758@columbia.edu>
2017-03-29 20:31:36 -0400

    material-UI

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -12,18 +12,49 @@ import cx from 'classnames';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Navigation.css';
 import Link from '../Link';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {blue300} from 'material-ui/styles/colors';
+
+
+const muiTheme = getMuiTheme({
+  borderRadius: 5,
+  palette: {
+    primary1Color: blue300,
+  },
+}, { userAgent: 'all' });
+
+const LogIn = () => (
+  <RaisedButton label="Log in" />
+);
+
+const SignUp = () => (
+  <RaisedButton 
+  label="Sign up"
+  primary={true}
+  />
+);
 
 class Navigation extends React.Component {
   render() {
     return (
+      <MuiThemeProvider muiTheme={muiTheme}>
       <div className={s.root} role="navigation">
         <Link className={s.link} to="/about">About</Link>
         <Link className={s.link} to="/contact">Contact</Link>
         <span className={s.spacer}> | </span>
-        <Link className={cx(s.link, s.lowlight)} to="/login">Log in</Link>
+        <LogIn />
+        {
+        // <Link className={cx(s.link, s.lowlight)} to="/login">Log in</Link>
+        }
         <span className={s.spacer}>or</span>
-        <Link className={cx(s.link, s.highlight, s.link.highlight)} to="/register">Sign up</Link>
+        <SignUp />
+        {
+        // <Link className={cx(s.link, s.highlight, s.link.highlight)} to="/register">Sign up</Link>
+        }
       </div>
+      </MuiThemeProvider>
     );
   }
 }
