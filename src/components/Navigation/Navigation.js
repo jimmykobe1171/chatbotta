309a1699bd07f51b19b52188fd9057936bb4a90f
TingMeng <tl2758@columbia.edu>
2017-04-07 17:50:41 -0400

    homepage img and js lint

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,14 +8,14 @@
  */
 
 import React from 'react';
-import cx from 'classnames';
+// import cx from 'classnames';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import s from './Navigation.css';
-import Link from '../Link';
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {blue300} from 'material-ui/styles/colors';
+import { blue300 } from 'material-ui/styles/colors';
+import s from './Navigation.css';
+import Link from '../Link';
 
 const muiTheme = getMuiTheme({
   borderRadius: 5,
@@ -29,67 +29,63 @@ class Navigation extends React.Component {
     super(props);
     this.logIn = this.logIn.bind(this);
     this.signUp = this.signUp.bind(this);
-    this.state = {logInPopUp: false, 
-      signUpPopUp: false};
+    this.state = { logInPopUp: false, signUpPopUp: false };
   }
 
-  logIn(){
-    this.setState({logInPopUp: true});
+  logIn() {
+    this.setState({ logInPopUp: true });
   }
 
-  signUp(){
-    this.setState({signUpPopUp: true});
+  signUp() {
+    this.setState({ signUpPopUp: true });
   }
 
-  logInButtonNormal(){
+  logInButtonNormal() {
     return (
-      <RaisedButton label="Log in" onClick={this.logIn} 
-      labelColor="#AFAFAF"/>
-    )
+      <RaisedButton label="Log in" onClick={this.logIn} labelColor="#AFAFAF" />
+    );
   }
 
-  logInButtonDisabled(){
-    return(
-        <RaisedButton label="Log in" disabled={true} />
-    )
-  }
-
-  signUpButtonNormal(){
+  logInButtonDisabled() {
     return (
-      <RaisedButton label="Sign up" onClick={this.signUp}
-        primary={true}/>
-    )
+      <RaisedButton label="Log in" disabled />
+    );
   }
 
-  signUpButtonDisabled(){
+  signUpButtonNormal() {
     return (
-      <RaisedButton label="Sign up"
-        primary={true} disabled={true}/>
-    )
+      <RaisedButton label="Sign up" onClick={this.signUp} primary />
+    );
+  }
+
+  signUpButtonDisabled() {
+    return (
+      <RaisedButton label="Sign up" primary disabled />
+    );
   }
 
   render() {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-      <div className={s.root} role="navigation">
-        <Link className={s.link} to="/about">About</Link>
-        <Link className={s.link} to="/contact">Contact</Link>
-        <span className={s.spacer}> | </span>
-        {(this.state.logInPopUp) ? this.logInButtonDisabled()
-                                : this.logInButtonNormal()}            
-        <span className={s.spacer}>or</span>
-        {(this.state.signUpPopUp) ? this.signUpButtonDisabled()
-                                : this.signUpButtonNormal()}        
+        <div className={s.root} role="navigation">
+          <Link className={s.link} to="/about">About</Link>
+          <Link className={s.link} to="/contact">Contact</Link>
+          <span className={s.spacer}> | </span>
+          {(this.state.logInPopUp) ? this.logInButtonDisabled()
+                                : this.logInButtonNormal()}
+          <span className={s.spacer}>or</span>
+          {(this.state.signUpPopUp) ? this.signUpButtonDisabled()
+                                : this.signUpButtonNormal()}
 
-        {(this.state.logInPopUp) ? 
-           <div className={s.logInPage}>
-           <h1>log in</h1>
-           </div> : null }   
-        {(this.state.signUpPopUp) ? 
-           <div className={s.signUpPage}>
-           <h1>sign up</h1>
-           </div> : null }
-      </div>
+          {(this.state.logInPopUp) ?
+            <div className={s.logInPage}>
+              <h1>log in</h1>
+            </div> : null }
+          {(this.state.signUpPopUp) ?
+            <div className={s.signUpPage}>
+              <h1>sign up</h1>
+            </div> : null }
+        </div>
       </MuiThemeProvider>
     );
   }
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -7,25 +7,25 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React, { PropTypes } from 'react';
+import React, { } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
+import robotImg from './robots-start.png';
 
 class Home extends React.Component {
   render() {
     return (
       <div className={s.root}>
         <div className={s.container}>
-          <img src="robots-start.png" width="350" height="380" alt="robots-start" />
+          <img src={robotImg} width="350" height="380" alt="robots-start" />
           <div className={s.passage}>
             <h1>Meet the bots that will make learning GREAT again</h1>
             <p>Studies that looked at delayed vs. immediate feedback discovered that participants
-          		who were given immediate feedback showed a significantly larger increase in 
-              performance than those who had received delayed feedback.
-          		<br /><br />
-				      Our chatbots enable students to get answers right away using machine learning 
-              to parse through course materials. TAs and faculty are also relieved of routine 
-              question-answering.</p>
+            who were given immediate feedback showed a significantly larger increase in
+            performance than those who had received delayed feedback.<br /><br />
+            Our chatbots enable students to get answers right away using machine learning
+            to parse through course materials. TAs and faculty are also relieved of routine
+            question-answering.</p>
           </div>
         </div>
       </div>
