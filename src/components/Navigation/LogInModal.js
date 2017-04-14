6a9bd1144edf0d0a1ef203274ecf3b5faccb8e32
Ethan Yuxuan Chen <ychen18@nd.edu>
2017-04-14 10:40:40 -0400

    login

--- src/components/Navigation/LogInModal.js
+++ src/components/Navigation/LogInModal.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
 import TextField from 'material-ui/TextField';
 import Checkbox from 'material-ui/Checkbox';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -11,20 +10,6 @@ const customContentStyle = {
   width: '380px',
   maxWidth: 'none',
 };
-
-const LogInTextField = () => (
-  <div>
-    <TextField
-      hintText="email"
-      fullWidth
-    /><br />
-    <TextField
-      hintText="password"
-      type="password"
-      fullWidth
-    /><br />
-  </div>
-);
 
 const CheckRemember = () => (
   <Checkbox
@@ -38,6 +23,9 @@ const CheckRemember = () => (
 export default class LogInModal extends React.Component {
   state = {
     open: false,
+    email: '',
+    password: '',
+    data: [],
   };
 
   handleOpen = () => {
@@ -48,42 +36,79 @@ export default class LogInModal extends React.Component {
     this.setState({ open: false });
   };
 
-  render() {
-    const actions = [
-      <FlatButton
-        label="Cancel"
-        onClick={this.handleClose}
-        labelStyle={{ color: '#AFAFAF' }}
-      />,
-      <FlatButton
-        label="Submit"
-        primary
-        type="submit"
-      />,
-    ];
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  }
 
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+  handleLoginSubmit = () => {
+    fetch('/api/login', {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+      }),
+      redirect: '/dashboard',
+    }).then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        console.log('LOGIN SUCCESS', response);
+        window.location.href = './dashboard';
+      } else {
+        const error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+      }
+    })
+      .then(response => response.json())
+      .catch(e => e);
+  }
+
+  render() {
     return (
       <div>
-        <form method="post">
-          <RaisedButton
-            label="Log in"
-            onClick={this.handleOpen}
-            labelColor="#AFAFAF"
-          />
-          <Dialog
-            title="Chatbot TA"
-            className={s.logInPage}
-            actions={actions}
-            modal={false}
-            contentStyle={customContentStyle}
-            open={this.state.open}
-            onRequestClose={this.handleClose}
-          >
-            <LogInTextField />
+        <RaisedButton
+          label="Log in"
+          onClick={this.handleOpen}
+          labelColor="#AFAFAF"
+        />
+        <Dialog
+          title="Chatbot TA"
+          className={s.logInPage}
+          modal={false}
+          contentStyle={customContentStyle}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          <form onSubmit={this.handleLoginSubmit}>
+            <TextField
+              hintText="email"
+              value={this.state.email}
+              onChange={this.handleEmailChange}
+              fullWidth
+            /><br />
+            <TextField
+              hintText="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handlePasswordChange}
+              fullWidth
+            /><br />
             <CheckRemember />
             <ForgotPassword />
-          </Dialog>
-        </form>
+            <button type="submit">Sign In</button>
+          </form>
+        </Dialog>
       </div>
     );
   }
