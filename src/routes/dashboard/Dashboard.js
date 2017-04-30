bf0c9ab8bc2eff448aead7d2f94dac60e9fb6a4b
Ethan Yuxuan Chen <ychen18@nd.edu>
2017-04-30 16:19:14 -0400

    refresh persistence

--- src/routes/dashboard/Dashboard.js
+++ src/routes/dashboard/Dashboard.js
@@ -16,6 +16,7 @@ import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import DashboardHeader from '../../components/DashboardHeader';
+import fetch from '../../core/fetch';
 import history from '../../core/history';
 import s from './Dashboard.css';
 
@@ -45,6 +46,34 @@ const textFieldStyle = {
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
+
+    fetch('/api/current-user', {
+      method: 'get',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      credentials: 'same-origin',
+    }).then((resp) => {
+      if (resp.status >= 200 && resp.status < 300) {
+        return resp.json();
+      }
+      const error = new Error(resp.statusText);
+      error.response = resp;
+      throw error;
+    })
+      .then((resp) => {
+        console.log('Dashboard - currentUser - SUCCESS', resp);
+        this.setState({
+          username: history.location.state.username,
+          courses: history.location.state.courses,
+        });
+      })
+      .catch((e) => {
+        console.log('Dashboard - currentUser - FAIL', e);
+        history.push('/');
+      });
+
     this.state = {
       dialog: [
         {
@@ -112,6 +141,8 @@ class Dashboard extends React.Component {
           content: 'The final is on May 8 at Mudd.',
         },
       ],
+      username: '',
+      courses: [],
     };
   }
 
@@ -129,36 +160,37 @@ class Dashboard extends React.Component {
       </div>);
     });
 
-    return (
-      <div>
-        <DashboardHeader
-          username={history.location.state.username}
-          courses={history.location.state.courses}
-        />
-        <MuiThemeProvider muiTheme={muiTheme}>
-          <Paper style={largePaperStyle} zDepth={4}>
-            <Paper style={smallPaperStyle} zDepth={2}>
-              <div className={s.dialogView}>
-                {dialogMessages}
-              </div>
-              <div className={s.textField}>
-                <TextField
-                  hintText="Your Message"
-                  multiLine
-                  rows={5}
-                  style={textFieldStyle}
-                />
-                <RaisedButton
-                  className={s.sendButton}
-                  label="Send"
-                  labelColor="#afafaf"
-                  primary
-                />
-              </div>
+    return (this.state.username ?
+        (<div>
+          <DashboardHeader
+            username={this.state.username}
+            courses={this.state.courses}
+          />
+          <MuiThemeProvider muiTheme={muiTheme}>
+            <Paper style={largePaperStyle} zDepth={4}>
+              <Paper style={smallPaperStyle} zDepth={2}>
+                <div className={s.dialogView}>
+                  {dialogMessages}
+                </div>
+                <div className={s.textField}>
+                  <TextField
+                    hintText="Your Message"
+                    multiLine
+                    rows={5}
+                    style={textFieldStyle}
+                  />
+                  <RaisedButton
+                    className={s.sendButton}
+                    label="Send"
+                    labelColor="#afafaf"
+                    primary
+                  />
+                </div>
+              </Paper>
             </Paper>
-          </Paper>
-        </MuiThemeProvider>
-      </div>
+          </MuiThemeProvider>
+        </div>) :
+        null
     );
   }
 }
